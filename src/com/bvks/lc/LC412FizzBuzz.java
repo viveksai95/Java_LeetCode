cbfba39a17cc9062685a791f9f73f551922c9462
Vivek <viveksai95@gmail.om>
2021-09-19 22:16:21 +0530

    LC - 9 - Palindrome Number

--- src/com/bvks/lc/LC412FizzBuzz.java
+++ src/com/bvks/lc/LC412FizzBuzz.java
@@ -3,7 +3,7 @@ package com.bvks.lc;
 import java.util.ArrayList;
 import java.util.List;
 
-public class N412FizzBuzz {
+public class LC412FizzBuzz {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
