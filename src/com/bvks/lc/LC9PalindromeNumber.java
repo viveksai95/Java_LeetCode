dc266eaf3eda93785d1395dc873ad3af440338e6
Vivek <viveksai95@gmail.om>
2021-09-19 22:18:19 +0530

    LC - 9 - Palindrome

---
+++ src/com/bvks/lc/LC9PalindromeNumber.java
@@ -0,0 +1,38 @@
+package com.bvks.lc;
+
+public class LC9PalindromeNumber {
+
+	public static void main(String[] args) {
+		isPalindromeV2(12344321);
+	}
+
+	// Time Complexity - O(log N)
+	public static boolean isPalindrome(int x) {
+		int reversedInput = 0;
+		int temp = x;
+
+		while (temp > 0) {
+			reversedInput = reversedInput * 10 + temp % 10;
+			temp /= 10;
+		}
+		System.out.println(reversedInput);
+
+		return reversedInput == x;
+	}
+
+	// Time Complexity - O(log N)
+	public static boolean isPalindromeV2(int input) {
+		if (input < 0 || input % 10 == 0 && input != 0)
+			return false;
+
+		int reversedNum = 0;
+
+		while (input > reversedNum) {
+			reversedNum = reversedNum * 10 + input % 10;
+			input /= 10;
+		}
+		System.out.println("x is : " + input);
+		System.out.println("reversedNum is : " + reversedNum);
+		return input == reversedNum || input == reversedNum / 10;
+	}
+}
