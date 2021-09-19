2da0406a164ebc6cb296a6d6aff507d4566ce86f
Vivek <viveksai95@gmail.om>
2021-09-19 21:41:14 +0530

    LC - 412 - FizzBuzz

---
+++ src/com/bvks/lc/N412FizzBuzz.java
@@ -0,0 +1,36 @@
+package com.bvks.lc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class N412FizzBuzz {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (String str : fizzBuzz(100)) {
+			System.out.println(str);
+		}
+	}
+
+	public static List<String> fizzBuzz(int n) {
+		List<String> outputList = new ArrayList<>();
+		for (int i = 1; i <= n; i++) {
+			outputList.add(calculateFizzBuzz(i));
+		}
+
+		return outputList;
+	}
+
+	public static String calculateFizzBuzz(int input) {
+		if (input % 5 == 0 && input % 3 == 0) {
+			return "FizzBuzz";
+		} else if (input % 3 == 0) {
+			return "Fizz";
+		} else if (input % 5 == 0) {
+			return "Buzz";
+		} else {
+			return Integer.toString(input);
+		}
+	}
+
+}
