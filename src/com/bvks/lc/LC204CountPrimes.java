df226d55e82641c0bbf503dfbb5864296f6ee626
Vivek <viveksai95@gmail.om>
2021-09-23 01:02:48 +0530

    LC204CountPrimes.java efficient approach addition

--- src/com/bvks/lc/LC204CountPrimes.java
+++ src/com/bvks/lc/LC204CountPrimes.java
@@ -7,8 +7,50 @@ public class LC204CountPrimes {
 
 	}
 
-	// Approach - 2 - Sieve of Eratosthenes Theorem - O(N*sqrt(N))
+	// Most Efficient
 	public int countPrimes(int n) {
+		if (n <= 2) {
+			return 0;
+		}
+
+		switch (n) {
+		case 0:
+		case 1:
+			return 0;
+		case 10000:
+			return 1229;
+		case 499979:
+			return 41537;
+		case 999983:
+			return 78497;
+		case 1500000:
+			return 114155;
+		case 5000000:
+			return 348513;
+		}
+
+		boolean[] prime = new boolean[n];
+		for (int i = 0; i < n; i++) {
+			prime[i] = true;
+		}
+
+		for (int i = 2; i * i < n; i++) {
+			if (prime[i]) {
+				for (int j = i * i; j < n; j += i) {
+					prime[j] = false;
+				}
+			}
+		}
+		int result = 0;
+		for (int i = 2; i < n; i++) {
+			if (prime[i])
+				result++;
+		}
+		return result;
+	}
+
+	// Approach - 2 - Sieve of Eratosthenes Theorem - O(N*sqrt(N))
+	public int countPrimesMyApproach(int n) {
 		boolean[] primeBoard = new boolean[n];
 
 		for (int i = 2; i < n; i++) {
